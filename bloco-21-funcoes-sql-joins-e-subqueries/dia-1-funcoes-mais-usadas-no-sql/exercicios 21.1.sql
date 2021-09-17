52feee5c093187bb42b6b31e70e35fa099b24667
rafaelprimon <rafaelfernandesprimon@gmail.com>
2021-09-17 19:20:37 -0300

    final dia 21.1

--- bloco-21-funcoes-sql-joins-e-subqueries/dia-1-funcoes-mais-usadas-no-sql/exercicios 21.1.sql
+++ bloco-21-funcoes-sql-joins-e-subqueries/dia-1-funcoes-mais-usadas-no-sql/exercicios 21.1.sql
@@ -46,10 +46,35 @@ UPDATE hr.employees
 SET phone_number = REPLACE(phone_number, '515', '777')
 WHERE phone_number LIKE '515%';
 
-/*  12. Escreva uma query que só exiba as informações dos funcionários cujo o primeiro nome tenha oito ou mais caracteres.
-13. Escreva uma query que exiba as seguintes informações de cada funcionário: id , primeiro nome e ano no qual foi contratado (exiba somente o ano).
-14. Escreva uma query que exiba as seguintes informações de cada funcionário: id , primeiro nome e dia do mês no qual foi contratado (exiba somente o dia).
-15. Escreva uma query que exiba as seguintes informações de cada funcionário: id , primeiro nome e mês no qual foi contratado (exiba somente o mês).
-16. Escreva uma query que exiba os nomes dos funcionários em letra maiúscula.
-17: Escreva uma query que exiba o sobrenome e a data de contratação de todos os funcionário contratados em julho de 1987.
-18: Escreva uma query que exiba as seguintes informações de cada funcionário: nome , sobrenome , tempo que trabalha na empresa (em dias) .
+/*  12. Escreva uma query que só exiba as informações dos funcionários cujo o primeiro nome tenha oito ou mais caracteres. */
+SELECT * FROM hr.employees
+WHERE LENGTH(FIRST_NAME) >= 8;
+
+/*  13. Escreva uma query que exiba as seguintes informações de cada funcionário: id , primeiro nome e ano no qual foi contratado (exiba somente o ano). */
+SELECT employee_id,
+first_name,
+year(hire_date) FROM hr.employees;
+
+/*  14. Escreva uma query que exiba as seguintes informações de cada funcionário: id , primeiro nome e dia do mês no qual foi contratado (exiba somente o dia). */
+SELECT employee_id,
+first_name,
+day(hire_date) FROM hr.employees;
+
+/*  15. Escreva uma query que exiba as seguintes informações de cada funcionário: id , primeiro nome e mês no qual foi contratado (exiba somente o mês). */
+SELECT employee_id,
+first_name,
+month(hire_date) FROM hr.employees;
+
+/* 16. Escreva uma query que exiba os nomes dos funcionários em letra maiúscula. */
+SELECT upper(first_name) FROM hr.employees;
+
+/* 17: Escreva uma query que exiba o sobrenome e a data de contratação de todos os funcionário contratados em julho de 1987. */
+SELECT last_name, hire_date
+FROM hr.employees
+WHERE MONTH(hire_date)=7 AND YEAR(hire_date)=1987;
+
+/*  18: Escreva uma query que exiba as seguintes informações de cada funcionário: nome , sobrenome , tempo que trabalha na empresa (em dias) . */
+SELECT first_name,
+last_name,
+DATEDIFF(CURRENT_DATE() , HIRE_DATE) AS 'tempo de empresa'
+FROM hr.employees;
