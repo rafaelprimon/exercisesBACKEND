d0aaccd66c013831c66cca93e12b0f8d83347fac
rafaelprimon <rafaelfernandesprimon@gmail.com>
2021-12-13 17:25:04 -0300

    ex 3 - rota post

--- bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -8,6 +8,20 @@ app.use(bodyParser.json());
 // exercicio 1
 app.get('/ping', (_req, res) => res.json({ "message": "pong" }));
 
+// exercicio 2
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(200).json({ "message": `Hello, ${name}` });
+});
+
+// exercicio 3
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+  if (age <= 17) { return res.status(401).json({ "message": "Unauthorized" });
+}
+  res.status(200).json({ "message": `Hello, ${name}` });
+});
+
 app.use(function (err, req, res, next) {
   res.status(500).send(`Erro? ${err.message}`);
 });
