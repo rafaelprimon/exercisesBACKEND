c3e94fd75f7d04e6ea2cf5e9cde8568d317e58b9
rafaelprimon <rafaelfernandesprimon@gmail.com>
2021-12-13 18:08:21 -0300

    ex 5 - cria api simpsons com modulo util implementado

---
+++ bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/simpsonIndex.js
@@ -0,0 +1,17 @@
+// exercicio 5
+const express = require('express');
+const bodyParser = require('body-parser');
+const rescue = require('express-rescue');
+
+const simpsonsUtils = require('./simpsonsUtils');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+app.use(function (err, req, res, next) {
+  res.status(500).send(`Erro? ${err.message}`);
+});
+
+app.listen(3000, () => console.log('listen port 3000!'));
