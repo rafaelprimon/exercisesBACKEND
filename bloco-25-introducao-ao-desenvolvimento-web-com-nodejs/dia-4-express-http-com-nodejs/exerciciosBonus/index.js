e8e4692321cc91ed76c658139133cd8d680ace07
rafaelprimon <rafaelfernandesprimon@gmail.com>
2021-12-13 19:03:26 -0300

    ex bonus 2 - acrescenta token aleatorio para cadastro

--- bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exerciciosBonus/index.js
+++ bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exerciciosBonus/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const auth = require('./authMiddleware');
+const token = require('./token');
 
 const app = express();
 
 app.use(express.json());
 app.use(auth);
 
+app.post('/signup', (req, res) => {
+  const { email, password, firstName, phone } = req.body;
 
+  if ([email, password, firstName, phone].includes(undefined)) {
+    return res.status(401).json({ "message": 'missing fields' });
+  }
+  const useToken = token.randomBytes(8).toString('hex');
+  res.status(200).json({ useToken});
+})
 
 app.use(function (err, req, res, next) {
   res.status(500).send(`erro: ${err.message}`);
